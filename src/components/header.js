eb5f42746955281a0769d8bf9a806aff5dfb342b
Uri <oriol.castroarnau@gmail.com>
2018-12-11 16:52:24 +0100

    Clean up and random stuff

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,39 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Typography from '@material-ui/core/Typography'
 
 const Header = ({ siteTitle }) => (
-  <div style={{ textAlign: 'center', margin: '8px' }}>
-    <Typography variant="h3" color="primary">
-      {siteTitle}
-    </Typography>
-  </div>
+  <StaticQuery
+    query={graphql`
+      query HeaderQuery {
+        file(name: { eq: "logo" }) {
+          childImageSharp {
+            fixed(height: 75) {
+              ...GatsbyImageSharpFixed_withWebp
+            }
+          }
+        }
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div style={{ textAlign: 'center', margin: '8px' }}>
+        {data.file ? (
+          <Img fixed={data.file.childImageSharp.fixed} />
+        ) : (
+          <Typography variant="h3" color="primary">
+            {data.site.siteMetadata.title}
+          </Typography>
+        )}
+      </div>
+    )}
+  />
 )
 
 Header.propTypes = {
