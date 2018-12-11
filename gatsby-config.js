862f45a61bbb1814d122a4d6554505237bf7acd6
Uri <oriol.castroarnau@gmail.com>
2018-12-11 17:49:31 +0100

    Small change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,5 @@
 const config = require('./src/meta/siteConfig')
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
