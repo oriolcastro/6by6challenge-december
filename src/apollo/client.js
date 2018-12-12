c1b878268439674331b9cae32eb8bea24b7271f5
Uri <oriol.castroarnau@gmail.com>
2018-12-12 13:49:56 +0100

    Add env variables again

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,9 +2,9 @@ import ApolloClient from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
 
 export const client = new ApolloClient({
-  uri: 'https://jumbocall-6by6december.herokuapp.com/v1alpha1/graphql',
+  uri: process.env.HASURA_GRAPHQL_ENDPOINT,
   fetch,
   headers: {
-    'X-Hasura-Access-Key': 'XcleNaUXeNtIcAuCIpLi',
+    'X-Hasura-Access-Key': process.env.HASURA_GRAPHQL_ACCESS_KEY,
   },
 })
