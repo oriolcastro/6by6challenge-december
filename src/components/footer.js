6d51731b663c554b57784dfc6530e518580953df
Uri <oriol.castroarnau@gmail.com>
2018-12-12 13:50:53 +0100

    Improve styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,20 @@ import Favorite from '@material-ui/icons/Favorite'
 import Typography from '@material-ui/core/Typography'
 
 const Footer = () => (
-  <div style={{ textAlign: 'center' }}>
+  <div
+    style={{ textAlign: 'center', backgroundColor: '#9E9E9E', padding: '8px' }}
+  >
     <Typography>
       Fet amb <Favorite color="error" fontSize="inherit" /> a Vilanova i la
-      Geltrú
+      Geltrú per{' '}
+      <a
+        href="https://okstudio.tech"
+        rel="nofollow noopener noreferrer"
+        target="_blank"
+      >
+        {' '}
+        Ok! Studio
+      </a>
     </Typography>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 
 import Layout from '../components/layout'
 import AppWrap from '../components/appwrap'
@@ -7,10 +8,19 @@ import withRoot from '../withRoot'
 
 const IndexPage = () => (
   <Layout>
-    <Typography gutterBottom align="justify" paragraph>
-      Fes-te una foto amb la càmera, escriu el teu missatge i publica-la perquè
-      aparegui a la pantalla gegant.
-    </Typography>
+    <Grid
+      container
+      alignItems="flex-start"
+      justify="space-around"
+      spacing="24px"
+    >
+      <Grid item lg="6" md="8" sm="10" xl="12" xs="12">
+        <Typography gutterBottom align="justify" paragraph>
+          Fes-te una foto amb la càmera, escriu el teu missatge i publica-la
+          perquè aparegui a la pantalla gegant.
+        </Typography>
+      </Grid>
+    </Grid>
     <AppWrap />
   </Layout>
 )
