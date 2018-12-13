6c69f339c7b5a422fdc1185b802d76114e5b97b2
Uri <oriol.castroarnau@gmail.com>
2018-12-13 10:29:42 +0100

    Small change

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
 
 export const client = new ApolloClient({
-  uri: process.env.GATSBY_HASURA_GRAPHQL_ENDPOINT,
+  uri: process.env.HASURA_GRAPHQL_ENDPOINT,
   fetch,
   headers: {
     'X-Hasura-Role': 'anonymous',
