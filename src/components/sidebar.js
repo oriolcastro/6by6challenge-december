75a5b15c0f012f47c0f6a5b18e7e54e68c366fa9
Uri <oriol.castroarnau@gmail.com>
2018-12-19 18:10:37 +0100

    Improve sidebar
    Adds QR code autogenerated with the defined siteUrl in meta.config

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,25 +1,51 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import QRCode from 'qrcode.react'
 
 import Footer from '../components/footer'
 import Header from '../components/header'
 
 const Sidebar = () => (
-  <Grid container direction="column" style={{ height: '100%' }}>
-    <Grid item style={{ marginTop: '48px' }}>
-      <Header />
-    </Grid>
-    <Grid item style={{ flexGrow: 1 }}>
-      <div style={{ margin: '48px' }}>
-        <Typography variant="h2">Hello!</Typography>
-        <Typography variant="h4">lorem impsum</Typography>
-      </div>
-    </Grid>
-    <Grid item>
-      <Footer />
-    </Grid>
-  </Grid>
+  <StaticQuery
+    query={graphql`
+      query SidebarQuery {
+        site {
+          siteMetadata {
+            siteUrl
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Grid container direction="column" style={{ height: '100%' }}>
+        <Grid item style={{ marginTop: '48px' }}>
+          <Header />
+        </Grid>
+        <Grid item style={{ flexGrow: 1 }}>
+          <div style={{ margin: '48px' }}>
+            <Typography paragraph variant="h2">
+              Benvingudes!
+            </Typography>
+            <Typography variant="h4" paragraph>
+              Per a publicar una imatge i que aparegui en pantalla ves a:{' '}
+              {data.site.siteMetadata.siteUrl}
+            </Typography>
+            <QRCode
+              value={data.site.siteMetadata.siteUrl}
+              renderAs="svg"
+              fgColor="#3f51b5"
+              size={200}
+            />
+          </div>
+        </Grid>
+        <Grid item>
+          <Footer />
+        </Grid>
+      </Grid>
+    )}
+  />
 )
 
 export default Sidebar
